54c662f77ab3a0ec6450440a3662bcf45563e98b
Kuznetsov, Roman <Roman.Kuznetsov@wolterskluwer.com>
2023-01-18 18:58:15 +0200

    Initial project structure

---
+++ src/main/java/com/bobocode/svydovets/App.java
@@ -0,0 +1,9 @@
+package com.bobocode.svydovets;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/com/bobocode/svydovets/AppTest.java
@@ -0,0 +1,14 @@
+package com.bobocode.svydovets;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    void stream() {
+        assertThat(true).isTrue();
+    }
+
+}
