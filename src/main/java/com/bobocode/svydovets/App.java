e417706dca06161bf3a3fc6cc2ba0728729f81ab
Kuznetsov, Roman <Roman.Kuznetsov@wolterskluwer.com>
2023-01-21 15:43:15 +0200

    BR-1 Add CheckStyle config to the project * added checkstyle.xml * updated pom.xml

--- src/main/java/com/bobocode/svydovets/App.java
+++ src/main/java/com/bobocode/svydovets/App.java
@@ -2,6 +2,9 @@ package com.bobocode.svydovets;
 
 public class App {
 
+    private App() {
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
