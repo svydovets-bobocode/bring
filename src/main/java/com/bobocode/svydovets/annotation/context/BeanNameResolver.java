9f119327544d5e0e7700be1fb4b0b76e7d5d69f9
Roman Kuznetsov <roman.kuznetsov@nure.ua>
2023-02-01 18:29:21 +0200

    BR-26 Update JavaDoc for BeanNameResolver

--- src/main/java/com/bobocode/svydovets/annotation/context/BeanNameResolver.java
+++ src/main/java/com/bobocode/svydovets/annotation/context/BeanNameResolver.java
@@ -2,7 +2,9 @@ package com.bobocode.svydovets.annotation.context;
 
 import java.lang.reflect.Method;
 
-//todo: add JavaDocs
+/**
+ * Provides name resolving capabilities for class-based and method-based Bring beans.
+ */
 public interface BeanNameResolver {
 
     String resolveBeanName(Class<?> type);
