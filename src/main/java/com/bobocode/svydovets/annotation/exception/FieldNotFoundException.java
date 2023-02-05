a24ff33fc52b8a70d3e4c76a2221abe8ff4884fe
ChonkaWork <chonka.work@gmail.com>
2023-02-05 17:52:19 +0200

    Merge remote-tracking branch 'origin/BR-33' into BR-33
    # Conflicts:
    #	src/main/java/com/bobocode/svydovets/annotation/context/AnnotationApplicationContext.java

--- src/main/java/com/bobocode/svydovets/annotation/exception/FieldNotFoundException.java
+++ src/main/java/com/bobocode/svydovets/annotation/exception/FieldNotFoundException.java
@@ -1,8 +1,12 @@
 package com.bobocode.svydovets.annotation.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class FieldNotFoundException extends RuntimeException {
     public FieldNotFoundException(String message) {
         super(message);
+        log.error(message);
     }
 
 }
